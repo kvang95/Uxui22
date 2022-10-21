8a5a807d182e0d4d308ec69331379843e50adbad
kvang95 <kabang95@hotmail.com>
2022-10-20 21:39:25 -0500

    19.2 activities

--- Copy of 19.2-activities/03_todo_app/unsolved/js/index.js
+++ Copy of 19.2-activities/03_todo_app/unsolved/js/index.js
@@ -1,5 +1,28 @@
 // Create event listener to enter items below
+$("#enter").on("click", function() {
+
+    var task = $("#todoItem").val();
+    $("#todoList").append("<div class='task'>" + task + "<div class='x fas fa-times'></div></div>");
+
+});
 
 // Create event listener to delete items below
+$(document).on("click", ".x", function() {
+
+    $(this).parent().remove();
+
+});
 
 // Create event listener to toggle 
+$(document).on("click", ".task", function() {
+    $(this).toggleClass("done");
+    if ($(this).hasClass("done")) {
+        
+
+}
+
+else {
+
+}
+
+}
